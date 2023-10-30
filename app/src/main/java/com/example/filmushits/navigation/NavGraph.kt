8584aec469d03521075fa1a71426d7817f98e2ab
wozhdeleniye <133259709+wozhdeleniye@users.noreply.github.com>
2023-10-30 13:26:54 +0700

    *

---
+++ app/src/main/java/com/example/filmushits/navigation/NavGraph.kt
@@ -0,0 +1,32 @@
+package com.example.filmushits.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.filmushits.log_reg_screens.LogRegScreen
+import com.example.filmushits.log_reg_screens.LogScreen
+import com.example.filmushits.log_reg_screens.RegScreen1
+import com.example.filmushits.log_reg_screens.RegScreen2
+
+
+@Composable
+fun Nav() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "LogRegScreen"){
+
+        composable(route = "LogRegScreen"){
+            LogRegScreen(navController)
+        }
+        composable(route = "LogScreen"){
+            LogScreen(navController)
+        }
+        composable(route = "RegScreen1"){
+            RegScreen1(navController)
+        }
+        composable(route = "RegScreen2"){
+            RegScreen2(navController)
+        }
+    }
+}
